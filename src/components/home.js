21394b5eeaebfba67ad2ea7ae8b3bd9018afb4fc
amanuel <dagetamanuel@gmail.com>
2024-10-10 22:00:17 +0300

    new change in resume

--- src/components/home.js
+++ src/components/home.js
@@ -26,7 +26,7 @@ export const Home = () => {
   };
   const handleDownload = () => {
       const link = document.createElement('a');
-      link.href = '/MyResume_4.pdf'; 
+      link.href = '/MyResume_6.pdf'; 
       link.download = 'AmanuelDaget_Resume.pdf'; 
       link.click();
   };
